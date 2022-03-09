df7c62abf6fa292b931f7f8f24606c1c6cf07875
Brandon May <brandonmay@Amiras-MacBook-Pro.local>
2022-03-08 21:21:30 -0500

    builds dependencies and adds template

---
+++ client/src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../Styles/Header.css';
+
+const Header = () => {
+    return (
+        <div className = 'header'>
+            <h3>Header</h3>
+        </div>
+    )
+}
+
+export default Header;
